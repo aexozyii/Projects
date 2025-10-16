2b98344410e6b721536d04b73ebbc7105978cbe9
aexozyii <seedaws3@gmail.com>
2025-10-16 11:22:23 +1100

    .

--- static/js/sw.js
+++ static/js/sw.js
@@ -2,14 +2,6 @@ const assets = [
   "/",
   "static/css/style.css",
   "static/js/app.js",
-  "static/images/logo.png",
-  "static/images/favicon.png",
-  "static/icons/icon-128x128.png",
-  "static/icons/icon-192x192.png",
-  "static/icons/icon-384x384.png",
-  "static/icons/icon-512x512.png",
-  "static/icons/desktop_screenshot.png",
-  "static/icons/mobile_screenshot.png",
 ];
 
 const CATALOGUE_ASSETS = "catalogue-assets";
